0bc351727f08d3179f9290e223f5a4f9e4173e7a
un-namedd <129755749+un-namedd@users.noreply.github.com>
2024-01-10 23:50:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,49 @@
 import os
 import discord
 import discord.ui
+import time
+import requests
+import json
 from discord.ui import View
 from discord.ext import commands
 from keep_alive import keep_alive
 
 keep_alive()
+
+client_id = '5vbnkp6tjtk9o2jy4kr9li372ja4dm'
+client_secret = '4uzclx2q4bxyfdz6oszymyrh8ckkjs'
+
+# Twitch streamer to check
+streamer = 'Sweetbuttcheeks'
+
+# Discord webhook URL
+webhook_url = 'https://discord.com/api/webhooks/1194732995324694588/flaW6OEgej-Sf1gK1FwLzHoRtZYehLThOK8YOxmWI6es7h3CDv3iU907kdR8vnfRQAOa'
+
+def get_twitch_token(client_id, client_secret):
+    url = 'https://id.twitch.tv/oauth2/token'
+    payload = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'grant_type': 'client_credentials'
+    }
+    response = requests.post(url, params=payload)
+    return response.json()['access_token']
+
+# Function to check if a streamer is online
+def is_online(streamer, client_id, token):
+    url = f'https://api.twitch.tv/helix/streams?user_login={streamer}'
+    headers = {'Client-ID': client_id, 'Authorization': f'Bearer {token}'}
+    response = requests.get(url, headers=headers)
+    return response.json()['data'] != []
+
+# Function to send a message to Discord
+def send_discord_message(content, webhook_url):
+    data = {'content': content}
+    response = requests.post(webhook_url, data=json.dumps(data), headers={"Content-Type": "application/json"})
+    return response.status_code
+
+# Get Twitch API access token
+token = get_twitch_token(client_id, client_secret)
 
 my_secret = 'MTE5MzUzOTY2MTc5NzI2NTQ4OA.G3z9S9.9VEY4HtK_SZzSEujuvpb88kpE_SKy0F0-SAapE'
 
@@ -208,3 +246,12 @@ async def menu(ctx):
   await ctx.send(embed=emb, view=view)
 
 bot.run('MTE5MzUzOTY2MTc5NzI2NTQ4OA.G3z9S9.9VEY4HtK_SZzSEujuvpb88kpE_SKy0F0-SAapE')
+
+while True:
+    if is_online(streamer, client_id, token):
+        if not message_sent:
+            send_discord_message(f'@Everyone {streamer} is now live on Twitch!', webhook_url)
+            message_sent = True
+    else:
+        message_sent = False  # Reset the flag when the streamer is offline
+    time.sleep(60)  # Check every minute
