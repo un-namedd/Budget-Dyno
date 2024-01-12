fdd8001437519dded1ccb6e06ecdd8b413eff466
un-namedd <129755749+un-namedd@users.noreply.github.com>
2024-01-13 01:18:24 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 def index():
     return "Alive"
 def run():
-  app.run(host='0.0.0.0',port=80)
+  app.run(host='0.0.0.0',port=8080)
 def keep_alive():  
     t = Thread(target=run)
     t.start()
