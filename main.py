0896acff00e200b1722d3d31f8242bfe9cabf4f0
un-namedd <129755749+un-namedd@users.noreply.github.com>
2024-05-10 20:09:24 +0300

    • Removed assign commands • Added role info command • Added only admin permission for initiaiting commands • Optimized code

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import discord
 import discord.ui
-from discord import ButtonStyle
-from discord.ui import View, Button
 from discord.ext import commands
-from keep_alive import keep_alive
-
-keep_alive()
 
 intents = discord.Intents().all()
 intents.messages = True
@@ -16,6 +11,7 @@ activity = discord.Activity(type=discord.ActivityType.watching, name="you rn.")
 
 bot = commands.Bot(command_prefix="!", intents = discord.Intents.all(), status = discord.Status.do_not_disturb, activity=activity)
 
+
 @bot.event
 async def on_ready():
     print("Bot is Up and Ready!")
@@ -25,273 +21,62 @@ async def on_ready():
     except Exception as e:
         print(e)
 
-
-
-@bot.tree.command(name="assignlane", description="Display the lane selection menu.")
-async def assignlane(interaction: discord.Interaction):
-    embed = discord.Embed(title="What role(s) do you play?",
-                          description="",
-                          colour=0xFF0000)
-    embed.set_author(
-        name="Intsuo",
-        icon_url="https://cdn.discordapp.com/avatars/769070942440914946/b1d3dc88c77b4d96adfc8a899e4c4838.webp?size=80")
-    embed.add_field(name="<:topl:1193209344968368189> Top",
-                    value="",
-                    inline=False)
-    embed.add_field(name="<:jungle:1193210201759817829> Jungle",
-                    value="",
-                    inline=False)
-    embed.add_field(name="<:mid:1193209803145752639> Mid",
-                    value="",
-                    inline=False)
-    embed.add_field(name="<:bot:1193209950462279900> Adc",
-                    value="",
-                    inline=False)
-    embed.add_field(name="<:supp:1193210180658274396> Support",
-                    value="",
-                    inline=False)
-    embed.add_field(name="<:fill:1193210237109420063> Fill",
-                    value="",
-                    inline=False)
-    await interaction.response.send_message(embed=embed, view=RoleView(), ephemeral=True)
-    
-
-
-class RoleButton(Button):
-    def __init__(self, role_name, emoji, row):
-        super().__init__(style=ButtonStyle.grey, label="", custom_id=role_name, emoji=emoji, row=row)
-        self.role_name = role_name
-
-    async def callback(self, interaction):
-        member = interaction.user
-        role = discord.utils.get(interaction.guild.roles, name=self.role_name)
-        if role in member.roles:
-            await member.remove_roles(role)
-            emb = discord.Embed(title="Role Update!",
-                                description=f"Removed {role.mention}",
-                                color=discord.Color.blue())
-            await interaction.response.send_message(embed=emb, ephemeral=True)
-        else:
-            await member.add_roles(role)
-            emb2 = discord.Embed(title="Role Update!",
-                                description=f"Added {role.mention}",
-                                color=discord.Color.blue())
-            await interaction.response.send_message(embed=emb2, ephemeral=True)
-
-
-
-class RoleView(View):
-    def __init__(self):
-        super().__init__(timeout=None)
-        self.add_item(RoleButton("Top", "<:topl:1193209344968368189>", row=0))
-        self.add_item(RoleButton("Jungle", "<:jungle:1193210201759817829>", row=0))
-        self.add_item(RoleButton("Mid", "<:mid:1193209803145752639>", row=0))
-        self.add_item(RoleButton("Adc", "<:bot:1193209950462279900>", row=1))
-        self.add_item(RoleButton("Support", "<:supp:1193210180658274396>", row=1))
-        self.add_item(RoleButton("Fill", "<:fill:1193210237109420063>", row=1))
-
-
-
-
-################################ ✅ Assign Rank COMMAND ✅ #################################
-################################ ✅ Assign Rank COMMAND ✅ #################################
-################################ ✅ Assign Rank COMMAND ✅ #################################
-################################ ✅ Assign Rank COMMAND ✅ #################################
-################################ ✅ Assign Rank COMMAND ✅ #################################
-
-
-
-
-@bot.tree.command(name="assignrank", description="Display the rank selection menu.")
-async def assignrank(interaction: discord.Interaction):
-    emb = discord.Embed(title="What's your current rank in the game?",
-                        description="",
-                        color=0xFF0000)
-    emb.set_author(
-    name="Intsuo",
-    icon_url="https://cdn.discordapp.com/avatars/769070942440914946/b1d3dc88c77b4d96adfc8a899e4c4838.webp?size=80")
-    view = MySelect()
-    view.timeout = None
-    await interaction.response.send_message(embed=emb, view=view, ephemeral=True)
-    
-class MySelect(View):
-    @discord.ui.select(
-        placeholder="Choose an option",
-        options=[
-            discord.SelectOption(label="Challenger",
-                                value="1",
-                                description="Pick this if you're Challenger.",
-                                emoji="<:challenger:1193213213152641124>"),
-            discord.SelectOption(label="Grandmaster",
-                                value="2",
-                                description="Pick this if you're Grandmaster.",
-                                emoji="<:grandmaster:1193213814586490980>"),
-            discord.SelectOption(label="Master",
-                                value="3",
-                                description="Pick this if you're Masters.",
-                                emoji="<:master:1193213238616272950>"),
-            discord.SelectOption(label="Diamond",
-                                value="4",
-                                description="Pick this if you're Diamond.",
-                                emoji="<:diamond:1193213838141693973>"),
-            discord.SelectOption(label="Emerald",
-                                value="5",
-                                description="Pick this if you're Emerald.",
-                                emoji="<:emerald:1193298992000401409>"),
-            discord.SelectOption(label="Platinum",
-                                value="6",
-                                description="Pick this if you're Platinum.",
-                                emoji="<:platinum:1193213880554500218>"),
-            discord.SelectOption(label="Gold",
-                                value="7",
-                                description="Pick this if you're Gold.",
-                                emoji="<:gold:1193213912439607316>"),
-            discord.SelectOption(label="Silver",
-                                value="8",
-                                description="Pick this if you're Silver.",
-                                emoji="<:silver:1193213895029035008>"),
-            discord.SelectOption(label="Bronze",
-                                value="9",
-                                description="Pick this if you're Bronze.",
-                                emoji="<:bronze:1193213933625032864>"),
-            discord.SelectOption(label="Iron",
-                                value="10",
-                                description="Pick this if you're Iron.",
-                                emoji="<:iron:1193213193212940429>"),
-            discord.SelectOption(label="Unranked",
-                                value="11",
-                                description="Pick this if you're Unranked."),
-            discord.SelectOption(label="Remove rank role",
-                                value="12",
-                                description="Pick this if you want to remove any rank you previously picked (from this list).",
-                                emoji="\u274C")
-        ])
-    async def select_callback(self, interaction, select):
-        select.disabled = True
-        user = interaction.user
-        guild = interaction.guild
-
-        # Define a list with the names of all rank roles
-        rank_roles = ["Unranked", "Iron", "Bronze", "Silver", "Gold", "Platinum", "Emerald", "Diamond", "Masters", "Grand Master", "Challenger"]
-
-        # Remove all existing rank roles from the user and keep track of the removed role
-        removed_role = None
-        for rank in rank_roles:
-            role = discord.utils.get(guild.roles, name=rank)
-            if role in user.roles:
-                await user.remove_roles(role)
-                removed_role = role
-
-        # Add the newly selected role to the user
-        new_role = None
-        if select.values[0] == "1":
-            new_role = discord.utils.get(guild.roles, name="Challenger")
-        elif select.values[0] == "2":
-            new_role = discord.utils.get(guild.roles, name="Grand Master")
-        elif select.values[0] == "3":
-            new_role = discord.utils.get(guild.roles, name="Masters")
-        elif select.values[0] == "4":
-            new_role = discord.utils.get(guild.roles, name="Diamond")
-        elif select.values[0] == "5":
-            new_role = discord.utils.get(guild.roles, name="Emerald")
-        elif select.values[0] == "6":
-            new_role = discord.utils.get(guild.roles, name="Platinum")
-        elif select.values[0] == "7":
-            new_role = discord.utils.get(guild.roles, name="Gold")
-        elif select.values[0] == "8":
-            new_role = discord.utils.get(guild.roles, name="Silver")
-        elif select.values[0] == "9":
-            new_role = discord.utils.get(guild.roles, name="Bronze")
-        elif select.values[0] == "10":
-            new_role = discord.utils.get(guild.roles, name="Iron")
-        elif select.values[0] == "11":
-            new_role = discord.utils.get(guild.roles, name="Unranked")
-
-        if new_role:
-            await user.add_roles(new_role)
-            if removed_role:
-                embed = discord.Embed(title="Rank Update!", description=f"Removed {removed_role.mention}!\n\nAdded {new_role.mention}!", color=discord.Color.blue())
-                await interaction.response.send_message(embed=embed, ephemeral=True)
-            else:
-                embed = discord.Embed(title="Rank Update!", description=f"Added {new_role.mention}!", color=discord.Color.blue())
-                await interaction.response.send_message(embed=embed, ephemeral=True)
-        elif select.values[0] == "12":
-            await interaction.response.send_message("Removed all ranks!", ephemeral=True)
-
-
-
-
-################################ ❌ Assign Rank END ❌ #################################
-################################ ❌ Assign Rank END ❌ #################################
-################################ ❌ Assign Rank END ❌ #################################
-################################ ❌ Assign Rank END ❌ #################################
-################################ ❌ Assign Rank END ❌ #################################
-
-
-
-################################ ✅ ROLES COMMAND ✅ #################################
-################################ ✅ ROLES COMMAND ✅ #################################
-################################ ✅ ROLES COMMAND ✅ #################################
-################################ ✅ ROLES COMMAND ✅ #################################
-################################ ✅ ROLES COMMAND ✅ #################################
-
-
+#########################################################################################################################################
+######################################################       Roles       ################################################################
+#########################################################################################################################################
 
 @bot.tree.command(name="role_remove", description="Remove a role from a user.")
 async def role_remove(interaction: discord.Interaction, member: discord.Member, role: discord.Role):
-    if interaction.user.id == 769070942440914946:
+    if discord.utils.get(interaction.user.roles, id=1145872122992930907):
         if role in member.roles:
             await member.remove_roles(role)
-            await interaction.response.send_message(f"Successfully removed role {role.name} from {member.name}.", ephemeral=True)
+            await interaction.response.send_message(f"Successfully removed role <@&{role.id}> from @{member.mention}.", ephemeral=True)
         else:
-            await interaction.response.send_message(f"{member.name} does not have the role {role.name}.", ephemeral=True)
+            await interaction.response.send_message(f"{member.mention} does not have the role <@&{role.id}>.", ephemeral=True)
     else:
         await interaction.response.send_message("You do not have permission to use this command.", ephemeral=True)
 
 @bot.tree.command(name="role_add", description="Add a role to a user.")
 async def role_add(interaction: discord.Interaction, member: discord.Member, role: discord.Role):
-    if interaction.user.id == 769070942440914946:
+    if discord.utils.get(interaction.user.roles, id=1145872122992930907):
         if role not in member.roles:
             await member.add_roles(role)
-            await interaction.response.send_message(f"Successfully added role {role.name} to {member.name}.", ephemeral=True)
+            await interaction.response.send_message(f"Successfully added role <@&{role.id}> to @{member.mention}.", ephemeral=True)
         else:
-            await interaction.response.send_message(f"{member.name} already has the role {role.name}.", ephemeral=True)
+            await interaction.response.send_message(f"{member.mention} already has the role <@&{role.id}>.", ephemeral=True)
     else:
         await interaction.response.send_message("You do not have permission to use this command.", ephemeral=True)
 
+@bot.tree.command(name="role_info", description="Get information about a role.")
+async def role_info(interaction: discord.Interaction, role: discord.Role):
+    if discord.utils.get(interaction.user.roles, id=1145872122992930907):
+        mention_text = f"<@&{role.id}>" if role.mentionable else "Not mentionable"
+        
+        embed = discord.Embed(title=f"Role Information:", color=role.color)
+        embed.add_field(name="ID", value=str(role.id), inline=False)
+        embed.add_field(name="Role Name", value=f"<@&{role.id}>", inline=False)
+        embed.add_field(name="Mention", value=mention_text, inline=False)
+        embed.add_field(name="Hoisted", value="Yes" if role.hoist else "No", inline=False)
+        embed.add_field(name="Position", value=str(role.position), inline=False)
+        embed.add_field(name="Mentionable", value="Yes" if role.mentionable else "No", inline=False)
+        permissions = [permission[0] for permission in role.permissions if permission[1]]
+        embed.add_field(name="Key Permissions", value=", ".join(permissions), inline=False)
 
+        # Format the creation date
+        creation_date = role.created_at.strftime("%B %d, %Y")
+        embed.set_footer(text=f"Role Created • {creation_date}")
 
-################################ ❌ ROLES COMMAND END ❌ #################################
-################################ ❌ ROLES COMMAND END ❌ #################################
-################################ ❌ ROLES COMMAND END ❌ #################################
-################################ ❌ ROLES COMMAND END ❌ #################################
-################################ ❌ ROLES COMMAND END ❌ #################################
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+    else:
+        await interaction.response.send_message("You do not have permission to use this command.", ephemeral=True)
 
-
+#########################################################################################################################################
+######################################################        Nick       ################################################################
+#########################################################################################################################################
 
 @bot.tree.command(name="nick", description="Change the nickname of a user.")
 async def nick(interaction: discord.Interaction, member: discord.Member, nickname: str):
-    if interaction.user.id == 769070942440914946:
         await member.edit(nick=nickname)
         await interaction.response.send_message(f"Successfully changed {member.name}'s nickname to {nickname}.", ephemeral=True)
-    else:
-        await interaction.response.send_message("You do not have permission to use this command.", ephemeral=True)
-
-@bot.tree.command(name="botnick", description="Change the nickname of the bot.")
-async def botnick(interaction: discord.Interaction, nickname: str):
-    if interaction.user.id == 769070942440914946:
-        await interaction.guild.me.edit(nick=nickname)
-        await interaction.response.send_message(f"Successfully changed the bot's nickname to {nickname}.", ephemeral=True)
-    else:
-        await interaction.response.send_message("You do not have permission to use this command.", ephemeral=True)
-
-
-
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    if message.content.startswith('hi'):
-        await message.channel.send('kys')
 
 bot.run('MTE5MzUzOTY2MTc5NzI2NTQ4OA.G3z9S9.9VEY4HtK_SZzSEujuvpb88kpE_SKy0F0-SAapE')
