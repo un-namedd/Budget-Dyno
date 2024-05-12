3e229535f8a2115bd731e9933c70a68b4d0738f3
un-namedd <129755749+un-namedd@users.noreply.github.com>
2024-05-12 11:59:08 +0300

    • Removed all comments. • Optimized Code • Added Embed'd commands instead of messages. • Fixed ID's.

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from keep_alive import keep_alive
 
 keep_alive()
 
+#Roles, User ID's
+admin_role_id = 1145872122992930907
+me = 769070942440914946
+
+
 intents = discord.Intents().all()
 intents.messages = True
 intents.reactions = True
@@ -24,35 +29,46 @@ async def on_ready():
         print(e)
 
 
-
 @bot.tree.command(name="role_remove", description="Remove a role from a user.")
 async def role_remove(interaction: discord.Interaction, member: discord.Member, role: discord.Role):
-    if discord.utils.get(interaction.user.roles, id=1145872122992930907):
+    if discord.utils.get(interaction.user.roles, id=admin_role_id):
         if role in member.roles:
             await member.remove_roles(role)
-            await interaction.response.send_message(f"Successfully removed role <@&{role.id}> from @{member.mention}.", ephemeral=True)
+            embed = discord.Embed(title="User Update:", color=role.color)
+            embed.add_field(name="Role Removed", value=f"Successfully removed role <@&{role.id}> from {member.mention}.", inline=False)
+            await interaction.response.send_message(embed=embed, ephemeral=True)
         else:
-            await interaction.response.send_message(f"{member.mention} does not have the role <@&{role.id}>.", ephemeral=True)
+            embed = discord.Embed(title="Error", color=role.color)
+            embed.add_field(name="User Role Not Found", value=f"{member.mention} does not have the role <@&{role.id}>.", inline=False)
+            await interaction.response.send_message(embed=embed, ephemeral=True)
     else:
-        await interaction.response.send_message("You do not have permission to use this command.", ephemeral=True)
+        embed = discord.Embed(title="Invalid Permissions.", color='black')
+        embed.add_field(name="Permission Required", value=("You do not have permission to use this command."), inline=False)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
 
 @bot.tree.command(name="role_add", description="Add a role to a user.")
 async def role_add(interaction: discord.Interaction, member: discord.Member, role: discord.Role):
-    if discord.utils.get(interaction.user.roles, id=1145872122992930907):
+    if discord.utils.get(interaction.user.roles, id=admin_role_id):
         if role not in member.roles:
             await member.add_roles(role)
-            await interaction.response.send_message(f"Successfully added role <@&{role.id}> to @{member.mention}.", ephemeral=True)
+            embed = discord.Embed(title="User Update:", color=role.color)
+            embed.add_field(name="Role Added", value=f"Successfully added role <@&{role.id}> from {member.mention}.", inline=False)
+            await interaction.response.send_message(embed=embed, ephemeral=True)
         else:
-            await interaction.response.send_message(f"{member.mention} already has the role <@&{role.id}>.", ephemeral=True)
+            embed = discord.Embed(title="Error", color=role.color)
+            embed.add_field(name="Role Error", value=f"{member.mention} already has the role <@&{role.id}>.", inline=False)
+            await interaction.response.send_message(embed=embed, ephemeral=True)
     else:
-        await interaction.response.send_message("You do not have permission to use this command.", ephemeral=True)
+        embed = discord.Embed(title=f"Invalid Permissions.", color='black')
+        embed.add_field(name="Permission Required", value=("You do not have permission to use this command."), inline=False)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
 
 @bot.tree.command(name="role_info", description="Get information about a role.")
 async def role_info(interaction: discord.Interaction, role: discord.Role):
-    if discord.utils.get(interaction.user.roles, id=1145872122992930907):
+    if discord.utils.get(interaction.user.roles, id=admin_role_id):
         mention_text = f"<@&{role.id}>" if role.mentionable else "Not mentionable"
         
-        embed = discord.Embed(title=f"Role Information:", color=role.color)
+        embed = discord.Embed(title=f"Role Information:", color=role.color) 
         embed.add_field(name="ID", value=str(role.id), inline=False)
         embed.add_field(name="Role Name", value=f"<@&{role.id}>", inline=False)
         embed.add_field(name="Mention", value=mention_text, inline=False)
@@ -62,19 +78,26 @@ async def role_info(interaction: discord.Interaction, role: discord.Role):
         permissions = [permission[0] for permission in role.permissions if permission[1]]
         embed.add_field(name="Key Permissions", value=", ".join(permissions), inline=False)
 
-        # Format the creation date
         creation_date = role.created_at.strftime("%B %d, %Y")
         embed.set_footer(text=f"Role Created • {creation_date}")
 
         await interaction.response.send_message(embed=embed, ephemeral=True)
     else:
-        await interaction.response.send_message("You do not have permission to use this command.", ephemeral=True)
-
+        embed = discord.Embed(title=f"Invalid Permissions.", color='black')
+        embed.add_field(name="Permission Required", value=("You do not have permission to use this command."), inline=False)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
 
 
 @bot.tree.command(name="nick", description="Change the nickname of a user.")
 async def nick(interaction: discord.Interaction, member: discord.Member, nickname: str):
+    if discord.utils.get(interaction.user.roles, id=admin_role_id):
         await member.edit(nick=nickname)
-        await interaction.response.send_message(f"Successfully changed {member.name}'s nickname to {nickname}.", ephemeral=True)
+        embed=discord.Embed(title="User Update:", color='black')
+        embed.add_field(name="Nick Change", value=f"Successfully changed {member.mention}'s nickname to {nickname}.", ephemeral=True)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+    else:
+        embed = discord.Embed(title=f"Invalid Permissions.", color='black')
+        embed.add_field(name="Permission Required", value=("You do not have permission to use this command."), inline=False)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
 
 bot.run('MTE5MzUzOTY2MTc5NzI2NTQ4OA.G3z9S9.9VEY4HtK_SZzSEujuvpb88kpE_SKy0F0-SAapE')
